fab72782f78c70c3f89e26a250a4e15fb2552961
Link Steve <steveshidifu@gmail.com>
2021-03-15 21:10:56 +0800

    更新异常相关文件

--- bin/SCC_Exception.py
+++ bin/SCC_Exception.py
@@ -1,6 +1,32 @@
-""""
-**模块说明** 软件的异常模块
-
-**模块状态** 开发中
 """
+**模块说明** 软件的异常模块，包含所有自定义模块抛出的异常
+
+**模块状态** 随模块的开发更新异常
+"""
+
 import SCC_Localization as Local
+
+
+# 底层数据库异常部分
+# SCC_Database
+
+
+class DBNotExistError(Exception):
+    """数据库不存在"""
+
+    def __str__(self):
+        return Local.GetTranslation('SCC_Database', 'DBNotExistError').translation
+
+
+class TableCreateError(Exception):
+    """表名已存在，无法新建"""
+
+    def __str__(self):
+        return Local.GetTranslation('SCC_Database', 'TableCreateError').translation
+
+
+class TableDropError(Exception):
+    """表名不存在，无法删除"""
+
+    def __str__(self):
+        return Local.GetTranslation('SCC_Database', 'TableDropError').translation
