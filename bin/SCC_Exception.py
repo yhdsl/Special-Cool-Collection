188a795aaceffc54ceaa9916ed6786de928a61d1
Link Steve <steveshidifu@gmail.com>
2021-04-03 15:35:23 +0800

    微调异常模块

--- bin/SCC_Exception.py
+++ bin/SCC_Exception.py
@@ -1,6 +1,5 @@
 """
-**模块说明** 软件的异常模块，包含所有自定义模块抛出的异常
-
+**模块说明** 软件的异常模块，包含 大部分自定义模块抛出的异常 \n
 **模块状态** 随模块的开发更新异常
 """
 
@@ -13,20 +12,17 @@ import SCC_Localization as Local
 
 class DBNotExistError(Exception):
     """数据库不存在"""
-
     def __str__(self):
         return Local.GetTranslation('SCC_Database', 'DBNotExistError').translation
 
 
 class TableCreateError(Exception):
     """表名已存在，无法新建"""
-
     def __str__(self):
         return Local.GetTranslation('SCC_Database', 'TableCreateError').translation
 
 
 class TableDropError(Exception):
     """表名不存在，无法删除"""
-
     def __str__(self):
         return Local.GetTranslation('SCC_Database', 'TableDropError').translation
